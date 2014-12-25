4dfcfd5095eac6344b11a7eea8d1020c7f0ba927
Piotr Esden-Tempski <piotr@esden.net>
2014-12-24 17:18:39 -0800

    We need to remove the JSBSim subdirectory for includes.
    Paparazzi expects the include files to be in the toplevel directory.

--- Formula/jsbsim.rb
+++ Formula/jsbsim.rb
@@ -12,8 +12,26 @@ class Jsbsim < Formula
   depends_on "pkgconfig"
   #depends_on "tcl"
 
+
+
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
+
+    inreplace ["src/Makefile.am",
+               "src/initialization/Makefile.am",
+               "src/input_output/Makefile.am",
+               "src/math/Makefile.am",
+               "src/models/Makefile.am",
+               "src/models/atmosphere/Makefile.am",
+               "src/models/flight_control/Makefile.am",
+               "src/models/propulsion/Makefile.am",
+               "src/simgear/Makefile.am",
+               "src/simgear/magvar/Makefile.am",
+               "src/simgear/misc/Makefile.am",
+               "src/simgear/props/Makefile.am",
+               "src/simgear/xml/Makefile.am",
+               "src/simgear/structure/Makefile.am"],
+      "includedir = @includedir@/JSBSim", "includedir = @includedir@"
 
     system "./autogen.sh"
     system "./configure", "--enable-libraries",
