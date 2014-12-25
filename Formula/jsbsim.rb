b38d9558fd037e3559825fe80b86a976a45a9ec5
Piotr Esden-Tempski <piotr@esden.net>
2014-12-24 17:17:55 -0800

    Rewound revision back to the one paparazzi still works with.
    The FGPropertyManager has been split into 2 classes, FGPropertyManager and FGPropertyNode.

--- Formula/jsbsim.rb
+++ Formula/jsbsim.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Jsbsim < Formula
   homepage "http://jsbsim.sourceforge.net"
-  url "git://git.code.sf.net/p/jsbsim/code", :revision => "47174755d6716bcf414933bded728fe5ab09ad70"
+  url "git://git.code.sf.net/p/jsbsim/code", :revision => "c4f7f7ce33aac0b0787a2df20acb05eeab6776b3"
   #sha1 ""
-  version "1.0-471747"
+  version "1.0-c4f747"
 
   depends_on "automake" => :build
   depends_on "autoconf" => :build
